165ea58330b30477c12bc763a8c9c69c142a1150
Reyaz <reyazoct@gmail.com>
2020-07-04 04:20:28 +0530

    - Add method getOppositeCommand to get get opposite direction

--- src/main/java/Command.kt
+++ src/main/java/Command.kt
@@ -4,6 +4,15 @@ enum class Command(val symbol: Char) {
     DOWN('d'),
     LEFT('l');
 
+    fun getOppositeCommand(): Command {
+        return when (this) {
+            UP -> DOWN
+            RIGHT -> LEFT
+            DOWN -> UP
+            LEFT -> RIGHT
+        }
+    }
+
     companion object {
         fun fromSymbol(symbol: Char): Command {
             return values().find { symbol == it.symbol } ?: throw Exception("Invalid Command symbol")
